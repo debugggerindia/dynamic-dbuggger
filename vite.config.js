c054f11f43a83fded140b54134d1b8c1491b55a2
debugggerindia <dbugggerofficial@gmail.com>
2024-11-25 22:37:37 +0530

    Add resolve.alias to Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // Optional: Specifies the directory for the production build
+  },
+  resolve: {
+    alias: {
+      // Add this alias to shorten imports from the 'src' folder
+      '@': '/src', // Now you can use '@' to reference the 'src' folder
+    },
+  },
 })
